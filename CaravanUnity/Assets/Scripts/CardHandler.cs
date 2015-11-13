9aad87e99bc5f6ae4b781440ea4cc12283728d03
Rob Nicholds <robert.james.nicholds@googlemail.com>
2015-11-13 16:45:54 +0000

    Card Textures

---
+++ CaravanUnity/Assets/Scripts/CardHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardHandler : MonoBehaviour
+{
+	[SerializeField] private int m_cardID;
+	[SerializeField] private Texture2D[] m_cardTextures;
+
+	private int MAX_CARD = 53;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
